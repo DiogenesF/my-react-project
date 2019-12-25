fd04f17451a691eaf39ff35925da0aaf3566403a
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2019-12-25 02:23:48 -0300

    Added SignUp

--- src/components/Accounts/SignUp.js
+++ src/components/Accounts/SignUp.js
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+const axios = require('axios').default;
 
+class SignUp extends React.Component {
+    state = {
+        name: "", email: "", password: "", confirm: ""
+    }
 
-function SignUp() {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.password !== this.state.confirm) {
+            alert("Your passwords didn't match!");
+        } else {
+            axios({
+                method: 'post',
+                url: "http://localhost:3001/acc",
+                data: {
+                    name: this.state.name,
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            }).then(res => {
+                if (res.ok) {
+                    alert("Your account was succesfully created!!");
+                }
+                else {
+                    var error = new Error("Error" + error.status + ":" + error.statusText);
+                    error.res = res;
+                    throw error;
+                }
+            }, 
+            error => {
+                var errMess = new Error(error.message);
+                throw errMess;
+            }).catch(error => {
+                console.log("Feedback couldn't be posted: " + error.message);
+                alert("Feedback couldn't be posted: " + error.message);
+            });
 
+            this.setState({ name: "", email: "", password: "", confirm: "" });
+        }
+    }
 
-    return (
-        <div className="container">
-            <h5>By creating an account here, you will be able to keep your own to-do list and access it
+    handleChange = (e) => {
+        this.setState({ ...this.state, [e.target.id]: e.target.value });
+    }
+
+    render() {
+        return (
+            <div style={{ backgroundColor: "lightCyan", padding: "1px" }}>
+                <div style={{ marginTop: "100px" }} className="container">
+                    <div style={{ padding: "0px 50px 0px 50px" }} className="row">
+                        <div className="col-12">
+                            <h5>By creating an account here, you will be able to keep your own to-do list and access it
                 anytime, and it's only available to you :D</h5>
-
-                <form class="col s6">
-                    <div class="row col s6"> 
-                        <label>Email</label>
-                        <input type="text" />
+                        </div>
+                        <div className="col-md-8 col-sm-12">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="name">Your name</label>
+                                    <input value={this.state.name} onChange={this.handleChange} type="name" className="form-control" id="name" placeholder="Your name..." />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Your email</label>
+                                    <input value={this.state.email} onChange={this.handleChange} type="email" className="form-control" id="email" placeholder="Your email..." />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input value={this.state.password} onChange={this.handleChange} type="password" className="form-control" id="password" placeholder="Your password..." />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="confirm">Confirm</label>
+                                    <input value={this.state.confirm} onChange={this.handleChange} type="password" className="form-control" id="confirm" placeholder="Confirm password..." />
+                                </div>
+                                <button style={{ marginBottom: "40px" }} type="submit" className="col-5 col-md-3 offset-4 btn btn-primary">Sign Up</button>
+                            </form>
+                        </div>
                     </div>
-                    <div class="row col s6"> 
-                        <label>Email</label>
-                        <input type="email" />
-                    </div> 
-                </form>
-        </div>
-    );
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SignUp;
