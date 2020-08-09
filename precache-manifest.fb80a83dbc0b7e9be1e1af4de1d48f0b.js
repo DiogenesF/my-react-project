9a7cd6bf9e768f9dfd6c5a27b86855b554c2789b
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-08-08 23:33:42 -0300

    Updates

---
+++ precache-manifest.fb80a83dbc0b7e9be1e1af4de1d48f0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b78c7365add27c3f2c2670e8ebfb4b7",
+    "url": "/my-react-project/index.html"
+  },
+  {
+    "revision": "a4918ac1019235fe2215",
+    "url": "/my-react-project/static/css/2.bed2e157.chunk.css"
+  },
+  {
+    "revision": "8ba5a854ff0b089ecae9",
+    "url": "/my-react-project/static/css/main.378eedf9.chunk.css"
+  },
+  {
+    "revision": "a4918ac1019235fe2215",
+    "url": "/my-react-project/static/js/2.a2910ec7.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/my-react-project/static/js/2.a2910ec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ba5a854ff0b089ecae9",
+    "url": "/my-react-project/static/js/main.28f0dcc6.chunk.js"
+  },
+  {
+    "revision": "a1fa09c062481197190c",
+    "url": "/my-react-project/static/js/runtime-main.8b58078e.js"
+  },
+  {
+    "revision": "ff2eb920a6a10c510f7cea185e5cb1f9",
+    "url": "/my-react-project/static/media/computer.ff2eb920.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-react-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-react-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-react-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-react-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-react-project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
