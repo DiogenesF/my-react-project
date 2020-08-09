f8c159dde12d591cd4bb33763d6ede35b49ac495
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-08-08 23:01:44 -0300

    Updating

--- src/components/MyContentComponent.js
+++ src/components/MyContentComponent.js
@@ -1,46 +1,72 @@
 import React from "react";
-import Background from "./styles/computer-science.png";
+import Background from "./styles/computer.jpeg";
 
 function MyContent() {
-
-    return (
-        <>
-            <div style={{ background: "url(" + Background + ") no-repeat center center fixed", backgroundSize: "cover", height: "100%", width:"100%" }}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12 text-center">
-                            <h3 style={{ marginTop: "80px", color: "white" }}><strong>Be welcome to my webpage!!</strong></h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12 mt-4 text-center">
-                            <img src="assets/images/my_image.png" alt="me" height="300px"/>
-                        </div>
-                    </div>
-                    <div className="row">
-
-                        <div className="col-sm-12 mt-4">
-                            <h5 style={{ color: "white" }}><strong>This is a website that I created using React just to practice and
-                            test my knowledge using this library. I didn't want to spend too much
-                            time with the design so I decided to keep it as simple as possible. Btw I used
-                    <a style={{ color: "gray" }} href="https://reactstrap.github.io/"> reactstrap</a> and a little inline CSS.</strong>
-                                <strong><br />A few details of the implementation: The state of the Rock-Paper-Scissors game won't
-                    change if you move through the routes, but in the Tic-Tac-Toe game it will.</strong>
-                                <strong>I am using Axios to store the accounts in a local json-server.</strong>
-                                <br /><strong>Up until now I used the following features: </strong>
-                                <ul style={{ marginLeft: "50px" }}>
-                                    <li><strong>React-Router-Dom</strong></li>
-                                    <li><strong>React-Hooks</strong></li>
-                                    <li><strong>Reactstrap</strong></li>
-                                    <li><strong>Axios</strong></li>
-                                </ul>
-                            </h5>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        style={{
+          background: "url(" + Background + ") no-repeat center center fixed",
+          backgroundSize: "cover",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h3 style={{ marginTop: "80px", color: "white" }}>
+                <strong>Be welcome to my webpage!!</strong>
+              </h3>
             </div>
-        </>
-    );
+          </div>
+          <div className="row">
+            <div className="col-sm-12 mt-4 text-center">
+              <img src="assets/images/my_image.png" alt="me" height="300px" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 mt-4">
+              <h5 style={{ color: "lightgray" }}>
+                <strong>
+                  This is a website that I created using React just to practice
+                  and test my knowledge using this library. I didn't want to
+                  spend too much time with the design so I decided to keep it as
+                  simple as possible. Btw I used
+                  <a
+                    style={{ color: "gray" }}
+                    href="https://reactstrap.github.io/"
+                  >
+                    {" "}
+                    reactstrap
+                  </a>{" "}
+                  and a little inline CSS.
+                </strong>
+                <strong>
+                  <br />A few details of the implementation: The state of the
+                  Rock-Paper-Scissors game won't change if you move through the
+                  routes, but in the Tic-Tac-Toe game it will.
+                </strong>
+                <br />
+                <strong>Up until now I used the following features: </strong>
+                <ul style={{ marginLeft: "50px" }}>
+                  <li>
+                    <strong>React-Router-Dom</strong>
+                  </li>
+                  <li>
+                    <strong>React-Hooks</strong>
+                  </li>
+                  <li>
+                    <strong>Reactstrap</strong>
+                  </li>
+                </ul>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MyContent;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import App from './App';
 import 'font-awesome/css/font-awesome.min.css';   //This is the font awesome 4, not 5
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
